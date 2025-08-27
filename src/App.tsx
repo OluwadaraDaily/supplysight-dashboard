43187a0dc6ca13eb9fb8151fe2884f8b73b3831d
Oluwadara Oloye <daraoloye99@gmail.com>
2025-08-27 23:08:16 +0100

    Add recharts dependency and implement KPI card component
    - Updated package.json and package-lock.json to include recharts version 3.1.2.
    - Created a new KpiCard component for displaying key performance indicators.
    - Modified App component to include a header and layout for KPI cards.
    - Adjusted CSS styles for App and index to improve layout and responsiveness.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import './App.css'
+import KpiCard from './components/kpi-card'
 
 function App() {
 
   return (
     <>
-      <h1>HOME!</h1>
+      <header className='p-4 shadow-md'>
+        <nav className='flex justify-between w-2/3 mx-auto'>
+          <p className='text-2xl font-bold'>SupplySight</p>
+          <div className='flex items-center gap-1'>
+            <button className='border-none cursor-pointer'>7d</button>
+            <span className='opacity-50'>/</span>
+            <button>14d</button>
+            <span className='opacity-50'>/</span>
+            <button>30d</button>
+          </div>
+        </nav>
+      </header>
+      <div className='flex gap-4 h-full'>
+        <aside className='max-w-[300px] w-[300px] h-full border border-red-500'></aside>
+        <main className='border border-green-900 w-full px-4'>
+          <div className='flex items-center gap-4'>
+            <KpiCard
+              title="Total Stock"
+              value={20000}
+            />
+            <KpiCard
+              title="Total Demand"
+              value={5000}
+            />
+
+            <KpiCard
+              title="Fill Rate"
+              value={1000}
+            />
+
+          </div>
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/kpi-card.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface IKpiCardProps {
+  title: string;
+  value: number
+}
+
+const KpiCard: React.FC<IKpiCardProps> = ({ title, value }) => {
+  return (
+    <div className="p-5 rounded-xl bg-gray-900 text-white w-[25%] mb-3 md:mb-0">
+        <p className="text-sm mb-3">{title}</p>
+        <p className="text-xl font-bold">{value}</p>
+      </div>
+  );
+};
+
+export default KpiCard;
